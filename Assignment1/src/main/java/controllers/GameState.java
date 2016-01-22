0e533750a2deb15aebfd0ec80e811a1943ee55c5
isaac-chan <isaac.chan13@gmail.com>
2016-01-21 16:30:38 -0800

    Changed deck and tests
    simpler implementation...

---
+++ Assignment1/src/main/java/controllers/GameState.java
@@ -0,0 +1,28 @@
+package controllers;
+import models.Deck;
+import sun.plugin.javascript.navig.Array;
+
+import java.util.ArrayList;
+
+/**
+ * Created by isaac on 1/21/2016.
+ */
+public class GameState {
+    Deck deck[];
+    void board(){
+
+    }
+
+    static void drawFromDeck(String deck[]) {
+        int deckIndex = 51;
+        for (int i = 0; i < deckIndex; i++) {
+            String drawnCard = deck[i];
+
+
+
+
+        }
+    }
+
+
+}
