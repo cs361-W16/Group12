b50d3dabf1a15363fbb3ebfe9432f5debe4a56bd
isaac-chan <isaac.chan13@gmail.com>
2016-01-21 23:31:13 -0800

    Completed Deck and Deal, commented tests
    Deal in gamestate, will need to add a "while" around the deal so it only
    deals after button is pressed
    
    Tests are commented for now since they aren't relevant

--- Assignment1/src/main/java/models/GameState.java
+++ Assignment1/src/main/java/models/GameState.java
@@ -8,26 +8,25 @@ import java.util.ArrayList;
  * Created by isaac on 1/21/2016.
  */
 public class GameState {
-    public static void main(String[] args){
-        Deck deck = new Deck();
+    public static void main(String[] args) {
 
-        static void drawFromDeck(String deck[]) {
-            int deckIndex = 51;
-            for (int i = 0; i < deckIndex; i++) {
-                String drawnCard = deck[i];
-            }
-        }
+        int numOfCardsInDeck = 52;
 
-        String[] col1 = {};
-        String[] col2 = {};
-        String[] col3 = {};
-        String[] col4 = {};
+        //initialize 4 columns of 13 spots each
+        String col1[] = new String[numOfCardsInDeck / 4];
+        String col2[] = new String[numOfCardsInDeck / 4];
+        String col3[] = new String[numOfCardsInDeck / 4];
+        String col4[] = new String[numOfCardsInDeck / 4];
 
-        static void dealCards(String deck[]){
-            col1[0] = drawnCard;
-        }
+        //initializes the deck of shuffled cards
+        Deck aDeck = new Deck(numOfCardsInDeck);
+
+        //run these 4 lines on button press
+        //while (pressedButton != 'close program') {
+            aDeck.drawFromDeck(col1);
+            aDeck.drawFromDeck(col2);
+            aDeck.drawFromDeck(col3);
+            aDeck.drawFromDeck(col4);
+        //}
     }
-
-
-
 }
