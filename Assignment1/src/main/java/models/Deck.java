5a46f709e9ac22e38ce85f5cf4945e86397f8dca
isaac-chan <isaac.chan13@gmail.com>
2016-01-21 22:08:21 -0800

    Organized and worked on deal inside gamestate

--- Assignment1/src/main/java/models/Deck.java
+++ Assignment1/src/main/java/models/Deck.java
@@ -4,13 +4,14 @@ package models;
  * Created by Isaac Chan on 1/20/2016.
  */
 public class Deck {
-    public static void deck(String[] args) {
+    public static void deck(String[] newDeck) {
         //Hardcoded array of 52 cards
-        String fullDeck[] = {"AH", "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "JH", "QH", "KH",
+        String[] fullDeck = {"AH", "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "JH", "QH", "KH",
                 "AD", "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "JD", "QD", "KD",
                 "AC", "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC",
                 "AS", "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "10S", "JS", "QS", "KS",};
 
+        newDeck = fullDeck;
         int N = 52;
 
         for (int i = 0; i < N; i++) {
---
+++ Assignment1/src/main/java/models/GameState.java
@@ -0,0 +1,33 @@
+package models;
+import models.Deck;
+import sun.plugin.javascript.navig.Array;
+
+import java.util.ArrayList;
+
+/**
+ * Created by isaac on 1/21/2016.
+ */
+public class GameState {
+    public static void main(String[] args){
+        Deck deck = new Deck();
+
+        static void drawFromDeck(String deck[]) {
+            int deckIndex = 51;
+            for (int i = 0; i < deckIndex; i++) {
+                String drawnCard = deck[i];
+            }
+        }
+
+        String[] col1 = {};
+        String[] col2 = {};
+        String[] col3 = {};
+        String[] col4 = {};
+
+        static void dealCards(String deck[]){
+            col1[0] = drawnCard;
+        }
+    }
+
+
+
+}
